580b98b93c79ebc9f47f03563773ed741505e125
cristina <Sorin@Gide.local>
2019-11-21 17:38:58 -0800

    First commit

---
+++ app.js
@@ -0,0 +1,63 @@
+const express= require("express");
+const bodyParser= require ("body-parser");
+const request= require("request");
+
+const app= express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.get("/", function(req,res){
+  res.sendFile(__dirname + "/signup.html");
+});
+app.post("/", function (req,res){
+  var firstName= req.body.fName;
+  var lastName= req.body.lName;
+  var email=req.body.email;
+
+  var data= {
+    members:[
+      {
+        email_address:email,
+        status: "subscribed",
+        merge_fields:{
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+  var jsonData=JSON.stringify(data);
+
+  var options={
+    url: "https://us15.api.mailchimp.com/3.0/lists/d8eca4ed2a",
+    method: "POST",
+    headers:{
+      "Authorization": "Practising f697f8281863a923afbe7329261d6d8c-us15"
+    },
+    body: jsonData
+  };
+
+  request(options,function(error,response,body){
+    if(error){
+      res.sendFile(__dirname+ "/failure.html");
+    }else { if(response.statusCode===200){
+      res.sendFile(__dirname+ "/success.html");
+    } else {
+      res.sendFile(__dirname+ "/failure.html");
+    }
+
+    }
+
+  });
+});
+app.post("/failure", function(req,res){
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server is running on port 3000");
+});
+
+//f697f8281863a923afbe7329261d6d8c-us15
+
+//d8eca4ed2a - id
